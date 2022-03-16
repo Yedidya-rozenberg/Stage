37d79ed37b71871087b9d8b6f5ffe56bc134f7c2
Yedidya-rozenberg <yedroz@gmail.com>
2022-03-16 13:03:57 +0200

    Pagination_API+add_component

--- StageApp/client/src/app/app-routing.module.ts
+++ StageApp/client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'users',
+    component: MembersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- StageApp/client/src/app/app.module.ts
+++ StageApp/client/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
