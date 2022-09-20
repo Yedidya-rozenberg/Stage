8fd51c565864e3964154ab844e00f92f33601cc0
Yedidya-rozenberg <yedroz@gmail.com>
2022-09-20 18:13:37 +0300

    updateCourse

--- StageApp/client/src/app/course/course.component.ts
+++ StageApp/client/src/app/course/course.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs';
 import { courseUpdate } from '../models/courseUpdate';
@@ -6,20 +6,31 @@ import { courseUnits } from '../models/courseUnits';
 import { AccountService } from '../services/account.service';
 import { CoursesService } from '../services/courses.service';
 import { course } from '../models/cours';
+import { NgForm, NgModel } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit  {
   course!: courseUnits;
   TeacherMode: boolean = false;
   editMode: boolean = false;
+  updateForm!: courseUpdate;
+@ViewChild('Form') form!: NgForm;
+
+
+
+  
 
   constructor(private route: ActivatedRoute,
     private coursesService: CoursesService,
-    private accountService: AccountService) { }
+    private accountService: AccountService,
+    private toastr: ToastrService
+    ) { }
+
 
   ngOnInit(): void {
     this.loadCourse();
@@ -28,6 +39,7 @@ export class CourseComponent implements OnInit {
   loadCourse() {
     this.coursesService.courentCourse$.pipe(take(1)).subscribe(
       course => { this.course = course as courseUnits })
+      this.updateForm = { courseStatus: this.course.details!.courseStatus, courseDescription: this.course.details!.courseDescription, courseName: this.course.details!.courseName };
   }
   isTeacher() {
     this.accountService.currentUser$.pipe(take(1)).subscribe(
@@ -35,10 +47,25 @@ export class CourseComponent implements OnInit {
   }
 
   switchStatus() {
-    const params: courseUpdate = { courseStatus: !this.course.details?.courseStatus, courseDescription: this.course.details?.courseDescription as string, courseName: this.course.details?.courseName as string };
-    this.coursesService.updateCourse(this.course.details?.courseID as number, params).subscribe();
+    this.updateForm.courseStatus = !this.course.details?.courseStatus;
+    this.coursesService.updateCourse(this.course.details?.courseID as number, this.updateForm).subscribe(
+      () => {
+        this.toastr.success("Course status updated");
+       }
+    );
     this.loadCourse();
   }
+
+  update(){
+    this.coursesService.updateCourse(this.course.details?.courseID as number, this.updateForm).subscribe(
+      () => {
+        this.toastr.success("Course successfully updated");
+      }
+    );
+    this.loadCourse();
+    this.form.reset();
+  }
+
 
 
 }
